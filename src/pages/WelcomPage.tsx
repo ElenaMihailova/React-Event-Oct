0508fdb008f98d21bfd3e891f25aa5f73040d642
Elena <mihailova.dev@gmail.com>
2024-10-24 16:03:50 +0200

    feat: add button

--- src/pages/WelcomPage.tsx
+++ src/pages/WelcomPage.tsx
@@ -1,9 +1,14 @@
 import { Container } from "@mui/material";
+import CustomButton from "../components/Button";
 
 export const WelcomePage = () => {
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
   return (
     <Container maxWidth="sm">
       Here we'll write something beautiful, concise, productive.
+      <CustomButton label="Click Me" onClick={handleClick} />
     </Container>
   );
 };
