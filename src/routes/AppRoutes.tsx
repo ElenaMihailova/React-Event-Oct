09517fbd00ba015b7099d659e9ee4254f295dc58
Iaroslav Rusanov <sco.nuff@gmail.com>
2024-10-26 17:11:51 +0300

    fix: del WelcomePage, Login is mainpage

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { WelcomePage } from "../pages/WelcomPage";
 import HelpRequestPage from "../pages/helpRequestPage/HelpRequestPage";
 import { Login } from "../pages/Login";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<WelcomePage />} />
+      <Route path="/" element={<Login />} />
       <Route path="/help-request" element={<HelpRequestPage />} />
-      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
