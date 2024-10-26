79490044943fe14e55dba8f6c9243332ab012ba6
Elena <mihailova.dev@gmail.com>
2024-10-27 00:55:41 +0200

    fix: deploy

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -10,6 +10,7 @@ import { ButtonIcon } from "../components/IconButton";
 import GridonRounded from "../assets/icon/GridonRounded.png";
 import ListAltRounded from "../assets/icon/ListAltRounded.png";
 import LocationOnFilled from "../assets/icon/LocationOnFilled.png";
+import { HelpRequestData } from '../types/types';
 
 export const CatalogPage: React.FC = () => {
   const theme = useTheme();
@@ -18,7 +19,7 @@ export const CatalogPage: React.FC = () => {
 
   const navigate = useNavigate();
 
-  const { data: cards = [], isLoading, error } = useGetRequestCardsQuery();
+  const { data: cards = [], isLoading, error } = useGetRequestCardsQuery(undefined);
   const totalPages = Math.ceil(cards.length / itemsPerPage);
 
   const [displayMode, setDisplayMode] = useState<"grid" | "list">("grid");
@@ -134,7 +135,7 @@ export const CatalogPage: React.FC = () => {
             ) : (
               <>
                 <ResultSection
-                  cards={paginatedCards.map((card) => ({
+                  cards={paginatedCards.map((card: HelpRequestData) => ({
                     ...card,
                     collectedAmount: card.requestGoalCurrentValue,
                     targetAmount: card.requestGoal,
