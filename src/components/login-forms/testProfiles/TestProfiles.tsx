abbdbb1ee60f0ca3ff72070a4cbecc2711a40cfd
Iaroslav Rusanov <sco.nuff@gmail.com>
2024-11-07 15:01:22 +0300

    fix: styles

--- src/components/login-forms/testProfiles/TestProfiles.tsx
+++ src/components/login-forms/testProfiles/TestProfiles.tsx
@@ -1,32 +1,13 @@
 import { Box, Typography } from "@mui/material";
 import InfoIcon from "../../../assets/login/IconInfo.png";
 import { testsProfilesData } from "./testProfilesData";
-
-const containerStyle: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  flexWrap: "wrap",
-  gap: "30px",
-  justifyContent: "start",
-};
-
-const cardStyle: React.CSSProperties = {
-  padding: "6px",
-  display: "flex",
-  flexDirection: "row",
-  gap: "14px",
-  width: "calc(25% - 16px)",
-  minWidth: "320px",
-  borderRadius: "6px",
-  border: "solid #0288D1",
-  color: "#014361",
-};
+import "./styles.css";
 
 export const TestProfiles: React.FC = () => {
   return (
-    <Box style={containerStyle}>
+    <Box className="container">
       {testsProfilesData.map((item) => (
-        <Box style={cardStyle} key={item.name}>
+        <div className="card" key={item.name}>
           <Box sx={{ padingLeft: 10 }}>
             <img src={InfoIcon} alt="Info" />
           </Box>
@@ -37,7 +18,7 @@ export const TestProfiles: React.FC = () => {
               <Typography variant="body1">{item.password}</Typography>
             </Box>
           </Box>
-        </Box>
+        </div>
       ))}
     </Box>
   );
